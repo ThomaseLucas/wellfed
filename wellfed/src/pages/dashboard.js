b22456d1f758f392965a7d4d35e3051fdb69275c
Daniel Castro <cas22032@byui.edu>
2025-03-01 01:15:12 -0700

    Updated dashboard

--- wellfed/src/pages/dashboard.js
+++ wellfed/src/pages/dashboard.js
@@ -6,7 +6,7 @@ export default function Dashboard() {
         <div className="bg-gray-100">
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/12 text-lg"
             >
-                <Link href="/index" scroll={false}>Home
+                <Link href="/" scroll={false}>Home
                 </Link>
             </button>
 
